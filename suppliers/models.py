8484c355b2f4aaa4febe1fbed8b6a99b83e65dac
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2020-08-26 00:47:06 -0300

    Creating front-end

--- suppliers/models.py
+++ suppliers/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Provider(models.Model):
-
     name = models.CharField(max_length=150, blank=False, null=False)
     cnpj = models.CharField(max_length=18, blank=False,
                             null=False, unique=True)
